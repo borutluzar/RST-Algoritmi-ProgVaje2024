fcdcf8749e0147f0f0bf0a95d065142a66cf4bbe
borutluzar <borut.luzar@gmail.com>
2024-02-22 15:12:14 +0100

    Add project files.

---
+++ RST-Algoritmi-ProgVaje2024/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RST_Algoritmi_ProgVaje2024
+{
+    /// <summary>
+    /// V ta projekt zapisujemo programsko kodo, 
+    /// ki jo razvijamo v sklopu predavanj in vaj 
+    /// pri predmetu Algoritmi na magistrskem študiju
+    /// Računalništva in spletnih tehnologij na 
+    /// Fakulteti za informacijske študije v Novem mestu.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // V prvem sklopu pripravljamo kodo za
+            // reševanje problema minimalnega vpetega drevesa v uteženih,
+            // neusmerjenih grafih.
+            Console.WriteLine("Minimalno vpeto drevo moramo še poiskati!");
+        }
+    }
+}
